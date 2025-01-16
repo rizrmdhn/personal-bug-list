158c40a19f3e00cb6173eb37abeead5ac62e1a99
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-01-17 07:29:15 +0800

    feat: Comment out AvatarImage component in NavUser for future reference

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -49,7 +49,7 @@ export function NavUser() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.username ?? ""} alt={user.username} />
+                {/* <AvatarImage src={user.username ?? ""} alt={user.username} /> */}
                 <AvatarFallback className="rounded-lg">
                   {user.username[0]?.toLocaleUpperCase()}
                 </AvatarFallback>
